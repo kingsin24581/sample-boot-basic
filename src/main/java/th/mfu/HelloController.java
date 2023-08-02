4ed54c2847a8ec438441a7b5bd52f8c2b62bcb25
Zodiac_ps <119025860+kingsin24581@users.noreply.github.com>
2023-08-02 14:29:04 +0700

    getMapping

--- src/main/java/th/mfu/HelloController.java
+++ src/main/java/th/mfu/HelloController.java
@@ -1,16 +1,26 @@
 package th.mfu;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
-
-    @RequestMapping("/hello")
+    @GetMapping("/hello")
     String hello() {
+
         return "Hello World!";
     }
 
-   
+    @GetMapping("/hello/{name}")
+    String hello(@PathVariable String name) {
+
+        return "Hello World!"+ name;
+    }
+
+   @GetMapping("/sum/{num1}/{num2}")
+   int sum(@PathVariable int num1 ,@PathVariable int num2){
+        return num1+num2;
+   }
 }
--- src/test/java/th/mfu/HelloControllerTest.java
+++ src/test/java/th/mfu/HelloControllerTest.java
@@ -5,19 +5,44 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @SpringBootTest
 public class HelloControllerTest {
 
     @Autowired
-    private HelloController controller;
+    private HelloController controller; //create controler class for Autowired
 
     @Test
     public void testHello() {
-
         // Act
         String response = controller.hello();
         // Assert
         assertEquals("Hello World!", response);
+        // Act
+        response = controller.hello("aomsin");
+        // Assert
+        assertEquals("Hello World!aomsin", response);
+    }
+
+    @Test
+    public void testSumNegativeNegative() {
+        
+        // Act
+        int response = controller.sum(-15 , -15);
+        // Assert
+        assertEquals(-30, response);
+        // Act
+        response = controller.sum(-8 , 15);
+        // Assert
+        assertEquals(7, response);
+        // Act
+        response = controller.sum(15 , 15);
+        // Assert
+        assertEquals(30, response);
+        // Act
+        response = controller.sum(15 , -15);
+        // Assert
+        assertEquals(0, response);
     }
 }
